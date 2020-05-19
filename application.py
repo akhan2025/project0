b4d55ad7a5825bb1752dc4dc6a82e89f4481ffff
Ali Khan <ahk21170@gmail.com>
2020-05-19 14:20:56 -0400

    python experiment

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='D:\intro to CS\project0\pics')
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/4year")
+def four_year():
+    return render_template("4year.html")
