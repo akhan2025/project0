b349c5dcb956e0c0033af55e298c5791fea40818
Ali Khan <ahk21170@gmail.com>
2020-05-20 14:28:57 -0400

    everything works for now

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__, static_folder='D:\intro to CS\project0\static')
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/4year")
+def four_year():
+    return render_template("4year.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route("/thankyou", methods=['GET', 'POST'])
+def thank_you():
+    name = request.form.get("name")
+    return render_template("thankyou.html", name=name)
